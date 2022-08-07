23fba6fa21aae95bb231df29b28484a509b29d2a
Anatolii Kasianov <tommy@macpaw.com>
2022-08-07 15:37:51 +0300

    Initial Commit

---
+++ MurConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MurConverter
+//
+//  Created by Anatolii Kasianov on 07.08.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
